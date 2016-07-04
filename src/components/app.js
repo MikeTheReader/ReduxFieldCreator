b9fa75137adbb2e0f5a67b7180bdf6b822a6e060
MikeTheReader <mlundin617@gmail.com>
2016-07-04 10:07:02 -0700

    Small style changes

--- src/components/app.js
+++ src/components/app.js
@@ -9,19 +9,18 @@ export default function App() {
         <div className="container-fluid">
             <h2>React / Redux Form Creator</h2>
             <div className="col-md-4">
-                <div className="panel panel-info">
-                    <div className="panel-heading">Selected Field</div>
-                    <div className="panel-body">
-                        <FieldForm />
-                    </div>
-                </div>
-            </div>
-            <div className="col-md-4">
-
                 <div className="panel panel-success">
                     <div className="panel-heading">Sample Form</div>
                     <div className="panel-body">
                         <SampleForm />
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-4">
+                <div className="panel panel-info">
+                    <div className="panel-heading">Selected Field</div>
+                    <div className="panel-body">
+                        <FieldForm />
                     </div>
                 </div>
             </div>
